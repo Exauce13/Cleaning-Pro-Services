547814b635671c40902e3c928e2b33ddabbd57bb
Exauce13 <jeanpaulgnikpo65@gmail.com>
2026-09-14 05:46:22 +0100

    Imagesrealisations

--- app/pages/a-propos.vue
+++ app/pages/a-propos.vue
@@ -69,7 +69,7 @@ const steps = [
         <div class="lg:col-span-6 relative">
           <div class="overflow-hidden rounded-3xl border border-slate-200 shadow-xl">
             <img
-              src="/images/cpsgroupe.jpg"
+              src="/images/cpsgroupe.jpeg"
               alt="Équipe Cleaning Pro Service"
               class="h-[280px] xs:h-[360px] sm:h-[420px] w-full object-cover"
             >
